f37098ad76cb975811d6e4f48aa7ebb0b4884d59
Ryan WS <jwestern@uoguelph.ca>
2021-05-04 16:50:33 -0400

    Split unit test into two

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ndarray_add_and_mul() {
+    fn test_ndarray_add() {
         let ones = Array::from_elem((2,2),1.);
         let twos = Array::from_elem((2,2),2.);
         let added = &twos + &ones;
-        let multd = &twos * &ones;
         assert_eq!(added, Array::from_elem((2,2),3.));
+    }
+
+    #[test]
+    fn test_ndarray_mul() {
+        let ones = Array::from_elem((2,2),1.);
+        let twos = Array::from_elem((2,2),2.);
+        let multd = &twos * &ones;
         assert_eq!(multd, Array::from_elem((2,2),2.));
     }
 }
