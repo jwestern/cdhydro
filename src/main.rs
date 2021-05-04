1e4fb92e8771329ec72c41607102d1d4a714efbd
Ryan WS <jwestern@uoguelph.ca>
2021-05-04 16:41:30 -0400

    Create unit test for simple ndarray operations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use ndarray::prelude::*;
 
 fn main() {
-    let _zeros = Array::from_elem((2,2),0.);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ndarray_add_and_mul() {
+        let ones = Array::from_elem((2,2),1.);
+        let twos = Array::from_elem((2,2),2.);
+        let added = &twos + &ones;
+        let multd = &twos * &ones;
+        assert_eq!(added, Array::from_elem((2,2),3.));
+        assert_eq!(multd, Array::from_elem((2,2),2.));
+    }
 }
