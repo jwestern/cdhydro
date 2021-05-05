7ca0b17c4d6d19c2ef965a508e7bde7349a4553e
Ryan WS <jwestern@uoguelph.ca>
2021-05-04 20:26:26 -0400

    ndarray-npy npz write/read unit test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ndarray::prelude::*;
-use ndarray_npy::{read_npy, write_npy, WriteNpyError, ReadNpyError, ReadNpyExt};
+use ndarray_npy::{read_npy, write_npy, NpzWriter, NpzReader};
+use std::fs::File;
 
 fn main() {
     let zeros = Array::from_elem((2,2),0.);
@@ -31,5 +32,20 @@ mod tests {
         write_npy("test.npy", &zeros);
         let recover: Array2<f64> = read_npy("test.npy").unwrap();
         assert_eq!(zeros, recover);
+    }
+
+    #[test]
+    fn test_ndarray_npz_read_write() {
+        let zeros = Array::from_elem((2,2),0.);
+        let time = Array::from_elem(1,0.5);
+        let mut npz = NpzWriter::new(File::create("test.npz").unwrap());
+        npz.add_array("data", &zeros);
+        npz.add_array("time", &time);
+        npz.finish();
+        let mut npzread = NpzReader::new(File::open("test.npz").unwrap()).unwrap();
+        let timeread: Array1<f64> = npzread.by_name("time").unwrap();
+        let dataread: Array2<f64> = npzread.by_name("data").unwrap();
+        assert_eq!(zeros, dataread);
+        assert_eq!(time,  timeread);
     }
 }
